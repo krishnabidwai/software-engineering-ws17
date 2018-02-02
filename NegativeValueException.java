3f0090f4947b3aa65e08539fbc3647f3976e305c
Krishna Bidwai <krish.bidwai@gmail.com>
2018-02-02 09:12:52 +0100

    Ass 7
    Exception Handling

---
+++ NegativeValueException.java
@@ -0,0 +1,35 @@
+
+public class NegativeValueException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NegativeValueException(String message) {
+
+		super(message);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+inputStr = base.inputStr;
+		outputStr = "Euro";
+		Conversionfactor = base.Conversionfactor * 0.85;
+
+
+
